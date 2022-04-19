cfc445242742cd55f1382758296d5c059a518e66
Wonjong Park <wonjong2@gmail.com>
2022-04-19 11:37:58 -0700

    Added the basic .prompt/.then/.catch structure of inquirer to index.js file

--- index.js
+++ index.js
@@ -1,2 +1,58 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'what is your project title?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Enter a description: ',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Enter a installation instructions: ',
+            name: 'install',
+        },
+        {
+            type: 'input',
+            message: 'Enter a usage information: ',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Enter a contribution guideline: ',
+            name: 'contributing',
+        },
+        {
+            type: 'input',
+            message: 'Enter a test instructions: ',
+            name: 'tests',
+        },
+        {
+            type: 'rawlist',
+            message: 'Choose a license for your application: ',
+            choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: 'Enter your GitHub username: ',
+            name: 'github',
+        },
+        {
+            type: 'input',
+            message: 'Enter your email address: ',
+            name: 'email',
+        },
+    ])
+    .then(response => 
+        console.log(response)
+    )
+    .catch(err => 
+        console.log(err)
+    );
