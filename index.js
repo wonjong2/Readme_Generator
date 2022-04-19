b5d9e7e83060d39f607c38cca6f9f2a906c44def
Wonjong Park <wonjong2@gmail.com>
2022-04-19 16:58:29 -0700

    Changed type in some of object : input -> editor

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ inquirer
             name: 'title',
         },
         {
-            type: 'input',
+            type: 'editor',
             message: 'Enter a description: ',
             name: 'description',
         },
         {
-            type: 'input',
+            type: 'editor',
             message: 'Enter a installation instructions: ',
             name: 'install',
         },
         {
-            type: 'input',
+            type: 'editor',
             message: 'Enter a usage information: ',
             name: 'usage',
         },
@@ -29,7 +29,7 @@ inquirer
             name: 'contributing',
         },
         {
-            type: 'input',
+            type: 'editor',
             message: 'Enter a test instructions: ',
             name: 'tests',
         },
@@ -38,6 +38,7 @@ inquirer
             message: 'Choose a license for your application: ',
             choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
             name: 'license',
+            pageSize: 8,
         },
         {
             type: 'input',
